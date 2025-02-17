b5b641b5d828e07462a83613e15ddaf42b881a80
Retr0_1995 <AskewGalaxy@gmail.com>
2025-02-17 11:43:24 +0000

    My test using an api
    Have fun

---
+++ my test document/my test document/Login.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace my_test_document
+{
+    public partial class Login : Form
+    {
+        string connection = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"C:\\Users\\M2300036\\OneDrive - Middlesbrough College\\my test document\\Test.mdf\";Integrated Security=True;Connect Timeout=30";
+
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string username, password;
+            username = txtUsername.Text;
+            password = txtPassword.Text;
+
+            
+            
+                SqlConnection conn = new SqlConnection(connection);
+                string query = ("SELECT * FROM Login WHERE Username = '" + txtUsername.Text + "' AND Password ='" + txtPassword.Text + "'");
+                SqlDataAdapter sda = new SqlDataAdapter(query, conn);
+
+                DataTable dtable = new DataTable();
+                sda.Fill(dtable);
+
+                if (dtable.Rows.Count > 0)
+                {
+                    username = txtUsername.Text;
+                    password = txtPassword.Text;
+
+                    MainTab MT = new MainTab(this);
+                    this.Hide();
+                    MT.ShowDialog();
+                    
+                    txtUsername.Clear();
+                    txtPassword.Clear();
+
+
+
+
+                }
+
+
+                else
+                {
+
+                    MessageBox.Show("invalid inputs");
+                    txtUsername.Clear();
+                    txtPassword.Clear();
+                }
+
+
+           
+
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            CreateAccount CA =new CreateAccount(this);
+            this.Hide();
+            CA.Show();
+        }
+    }
+}
---
+++ my test document/my test document/MainTab.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Serialization;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TaskbarClock;
+
+namespace my_test_document
+{
+    public partial class MainTab : Form
+    {
+       
+        Form OldForm;
+        public MainTab(Form PreviousForm)
+        {
+            InitializeComponent();
+            OldForm = PreviousForm;
+        }
+
+        private DateTime TIME;
+        private string TEMP;
+        private string WIND;
+        private string RAIN;
+
+        private void btnLogOut_Click(object sender, EventArgs e)
+        {
+            OldForm.Show();
+            this.Close();
+        }
+
+        private void btnreview_Click(object sender, EventArgs e)
+        {
+            string APIKey = "9031f4d8c80278427290c4f63b28199f";
+            string Location =txtLocation.Text;
+            string URL = $"http://api.openweathermap.org/data/2.5/forecast?&mode=xml&q={Location}&units=metric&APPID={APIKey}";
+            
+            WebClient client = new WebClient();
+
+            DisplayForecast(client.DownloadString(URL));
+        }
+
+        private void DisplayForecast(string xml)
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.LoadXml(xml);
+
+            // Get the city, country, latitude, and longitude.
+            XmlNode locNode = xmlDoc.SelectSingleNode("weatherdata/location");
+            txtLocation.Text = locNode.SelectSingleNode("name").InnerText;
+
+            foreach (XmlNode timeNode in xmlDoc.SelectNodes("//time"))
+            {
+                // Get the time in UTC.
+                TIME =
+                    DateTime.Parse(timeNode.Attributes["from"].Value);
+
+                // Get the temperature.
+                XmlNode tempNode = timeNode.SelectSingleNode("temperature");
+                TEMP = tempNode.Attributes["value"].Value;
+
+                XmlNode windNode = timeNode.SelectSingleNode("windSpeed");
+                WIND = windNode.Attributes["mps"].Value;
+
+                XmlNode rainnode = timeNode.SelectSingleNode("precipitation");
+                RAIN = rainnode.Attributes["probability"].Value;
+                //Display the data
+
+
+
+
+
+                ListViewItem listViewItem = new ListViewItem(TIME.DayOfWeek.ToString());
+                listViewItem.SubItems.Add(TIME.ToShortTimeString());
+                listViewItem.SubItems.Add(TEMP);
+                listViewItem.SubItems.Add(WIND);
+                listViewItem.SubItems.Add(RAIN);
+
+                list.Items.Add(listViewItem);
+
+            }
+
+        }
+    }
+}
